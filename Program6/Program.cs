8e6a647910bf229d481d69a00b6aaa198fc807b1
Mayank-Shekhar09 <mayank.shekhar@ltts.com>
2021-07-08 15:55:07 +0530

    Program5 SoloLearn.

---
+++ Program6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+            foreach (string word in words)
+            {
+                if (word.Contains(letter))
+                {
+                    count++;
+                    Console.WriteLine(word);
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No match");
+            }
+
+            //your code goes here
+
+        }
+    }
+}
