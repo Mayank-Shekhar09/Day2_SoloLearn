1292bc85707666e4c383b9e0e202954ea102dc18
Mayank-Shekhar09 <mayank.shekhar@ltts.com>
2021-07-08 17:10:47 +0530

    Program 8 Solo Learn.

---
+++ Program8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+
+                //your code goes here
+                int div = drinks / shelves;
+                Console.WriteLine(div);
+
+            }
+            /*
+             * 1. DivideByZeroException => "At least 1 shelf"
+             * 2. FormatException => "Please insert an integer"
+            */
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("At least 1 shelf");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please insert an integer");
+            }
+        }
+    }
+}
