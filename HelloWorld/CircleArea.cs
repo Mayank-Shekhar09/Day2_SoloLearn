6f513196ae2a13492ae3dd9a5fcf78d34deba6b1
Mayank-Shekhar09 <mayank.shekhar@ltts.com>
2021-07-08 13:10:18 +0530

    Area of Circle, Program 2.

---
+++ HelloWorld/CircleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class CircleArea
+    {
+        public void Area()
+        {
+            const double pi = 3.14;
+            double radius;
+            radius = Convert.ToDouble(Console.ReadLine());
+            double area = pi * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+}
